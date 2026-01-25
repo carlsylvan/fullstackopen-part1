9d19a222560541f9f11884b75b04a6946384e739
Carl Sylvan <carlsylvan@gmail.com>
2026-01-25 15:18:52 +0100

    1.7: unicafe step 2

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -6,10 +6,63 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleClick = (setFeedback) => {
+    setFeedback(prev => prev + 1)
+  }
+
+ const countAll = () => {
+      return good + neutral + bad
+  }
+
+   const countAverage = () => {
+    const goodScore = 1
+    const neutralScore = 0
+    const badScore = -1
+
+    const totalScore = good + neutral + bad
+
+    if (totalScore === 0) {
+      return 0
+    }
+
   return (
-    <div>
-      code here
+    good * goodScore +
+    neutral * neutralScore +
+    bad * badScore
+  ) / totalScore
+  }
+
+
+  const positivePercentage = () => {
+
+    const totalScore = good + neutral + bad
+
+    if (totalScore === 0) return 0
+
+    return (good / totalScore) * 100
+  }
+
+
+  return (
+    <>
+    <div id="feedback">
+        <h2>give feedback</h2>
+
+      <button onClick={() => handleClick(setGood)}>good</button>
+      <button onClick={() => handleClick(setNeutral)}>neutral</button>
+      <button onClick={() => handleClick(setBad)}>bad</button>
     </div>
+
+    <div id="statistics">
+          <h2>statistics</h2>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {countAll()}</div>
+      <div>average {countAverage()}</div>
+      <div>positive {positivePercentage()}</div>
+    </div>
+    </>
   )
 }
 
